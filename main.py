0a6ef7b9581f8e8388c722c126a865bb3b96dcac
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-04-07 15:25:04 -0600

    fix: new module curtain was implemented
    Old code was replaced for the new "curtain" moduele

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-from machine import PWM, Pin
-from micropython_servo_pdm_360 import ServoPDM360
-import time
+import gc
 
-# create a PWM servo controller (21 - pin Pico)
-servo_pwm = PWM(Pin(12))
-
-# Set the parameters of the servo pulses, more details in the "Documentation" section
-freq = 50
-min_us = 400
-max_us = 2550
-dead_zone_us = 150
-
-# create a servo object
-servo = ServoPDM360(
-    pwm=servo_pwm, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
-)
+import config
+import curtain
 
 if __name__ == "__main__":
-    while True:
-        servo.turn_cv(4)
-        time.sleep(2)
-        servo.turn_ccv(4)
-        time.sleep(2)
+    c = None
+
+    try:
+        c = curtain.Curtain(config.SERVO_PIN, config.LIGHT_SENSOR_PIN, 16)
+        c.start()
+    except KeyboardInterrupt:
+        print("System interrumpet by the user")
+    except Exception as e:
+        print(f"Fatal erro: {e}")
+    finally:
+        if c:
+            c.stop()
+        gc.collect()
